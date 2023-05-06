6edfb936de5d6a6dd9435f58ea83fbaadc772c09
Afonso Lima <afonsolima@gmail.com>
2023-05-06 18:11:04 -0300

    - restaurando arquivos do projeto_base (outra vez)

--- src/main/java/me/dio/academia/digital/repository/AvaliacaoFisicaRepository.java
+++ src/main/java/me/dio/academia/digital/repository/AvaliacaoFisicaRepository.java
@@ -1,10 +1,4 @@
 package me.dio.academia.digital.repository;
 
-import me.dio.academia.digital.entity.AvaliacaoFisica;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AvaliacaoFisicaRepository extends JpaRepository<AvaliacaoFisica, Long> {
-
+public interface AvaliacaoFisicaRepository {
 }
